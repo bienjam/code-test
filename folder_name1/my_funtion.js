f8b5efa45634b5c1de409937ad57bb688bd0ed58
bienjam <jamonbienvenido@gmail.com>
2023-01-02 23:40:28 +0800

    2nd-try

---
+++ folder_name1/my_funtion.js
@@ -0,0 +1,4 @@
+function my_function (data, result) {
+  // do something (it'll take sometime, you can use setTimeout() here, so that delay the time, for example.
+  return result; }
+  module.exports = my_function;
